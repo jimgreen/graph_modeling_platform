c1102a19e8c466d6bc195b93bc372fdce58181f6
foree <foree@foxmail.com>
2026-08-31 19:33:41 +0800

    style(measurement): value tspan x 设为 15

--- src/appExtracted/appToolbarHookFactories.tsx
+++ src/appExtracted/appToolbarHookFactories.tsx
@@ -322,7 +322,7 @@ export function createRenderMeasurementGroup(__appScope: Record<string, any>) {
               <tspan
                 id={`mv-${row.item.id}`}
                 className="measurement-value mv"
-                x={formatSvgNumber(valueX)}
+                x="15"
                 textAnchor="start"
                 xmlSpace="preserve"
               >
